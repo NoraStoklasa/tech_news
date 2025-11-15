c8a63f982dfc5d886662d7c81a209dc144fafb38
Nora <stoklasa.nora@gmail.com>
2025-11-15 20:42:36 +1000

    Starting over with scraping two websites

--- main.py
+++ main.py
@@ -8,47 +8,27 @@ load_dotenv()
 client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-tc_url = "https://techcrunch.com/"
+news_dict = {
+    "tech-crunch": "https://techcrunch.com/",
+    "the-verge": "https://www.theverge.com/",
+}
 
 
-tc_response = requests.get(tc_url)
+articles = []
+for website, url in news_dict.items():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-tc_soup = BeautifulSoup(tc_response.text, "html.parser")
-
-tc_titles = tc_soup.find_all("a", class_="loop-card__title-link")
-
-links = {}
-
-for title in tc_titles:
-    links[title.text.strip()] = title["href"]
-
-# print(links)
-
-
-url = next(iter(links.values()))
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-paragraphs = soup.find_all("p", class_="wp-block-paragraph")
-article_text = " ".join(p.get_text() for p in paragraphs)
-
-# print(article_text)
+    if website == "tech-crunch":
+        articles.extend(soup.find_all("a", class_="loop-card__title-link"))
+    elif website == "the-verge":
+        articles.extend(soup.find_all("a", class_="_1lkmsmo0 _1lksmo4"))
+    else:
+        print("error")
 
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "You summarize text clearly and simply for computer science students.",
-        },
-        {
-            "role": "user",
-            "content": f"Summarise this article in beginner-friendly language:\n\n{article_text}",
-        },
-    ],
-    max_tokens=200,
-    temperature=0.3,
-)
-summary = response.choices[0].message.content
+article_dict = {}
+for article in articles:
+    article_dict[article.text.strip()] = article["href"]
 
-print(summary)
+print(article_dict)
